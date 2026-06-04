ef4ec6f45713cc3b49a00b3e55ed267fa6c7f0dd
danidigrandi019 <digrandi56@gmail.com>
2026-06-04 12:59:42 -0300

    feat: plano de leitura na home e menu mobile com fechar fora

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HeroComponent } from '../../hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
